4a72de6d331e7126e3cca7cddf4befd76ccf7e50
henriquelima23 <69582368+henriquelima23@users.noreply.github.com>
2020-10-07 11:19:00 -0300

    Update web.py

--- web.py
+++ web.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
- return "Henrique Lima"
+ return "meu primeiro pipeline"
 if __name__ == "__main__":
  port = int(os.environ.get("PORT", 5000))
  app.run(host='0.0.0.0', port=port)
